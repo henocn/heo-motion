fcec127402461aa21d0f5dd09af516066d25bacc
Henoc N'GASAMA <ngasamah@gmail.com>
2026-04-21 15:31:40 +0000

    change port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: 5175,
+    port: 5174,
     proxy: {
       "/api": {
         target: "http://localhost:8000",
